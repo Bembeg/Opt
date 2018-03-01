e8c5216db0e3ebf3f3bef6052f8748c7c0a36f14
Bembeg <bembegcz@centrum.cz>
2018-03-01 21:08:39 +0100

    Initial commit, testing the Git system

---
+++ Build/CMakeFiles/Opt.dir/DependInfo.cmake
@@ -0,0 +1,67 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/b/Documents/Dropbox/Programování/Geant4/Opt/Opt.cc" "/home/b/Documents/Dropbox/Programování/Geant4/Opt/Build/CMakeFiles/Opt.dir/Opt.cc.o"
+  "/home/b/Documents/Dropbox/Programování/Geant4/Opt/src/OptActionInitialization.cc" "/home/b/Documents/Dropbox/Programování/Geant4/Opt/Build/CMakeFiles/Opt.dir/src/OptActionInitialization.cc.o"
+  "/home/b/Documents/Dropbox/Programování/Geant4/Opt/src/OptDetectorMessenger.cc" "/home/b/Documents/Dropbox/Programování/Geant4/Opt/Build/CMakeFiles/Opt.dir/src/OptDetectorMessenger.cc.o"
+  "/home/b/Documents/Dropbox/Programování/Geant4/Opt/src/OptPhysicsList.cc" "/home/b/Documents/Dropbox/Programování/Geant4/Opt/Build/CMakeFiles/Opt.dir/src/OptPhysicsList.cc.o"
+  "/home/b/Documents/Dropbox/Programování/Geant4/Opt/src/OptPrimaryGeneratorAction.cc" "/home/b/Documents/Dropbox/Programování/Geant4/Opt/Build/CMakeFiles/Opt.dir/src/OptPrimaryGeneratorAction.cc.o"
+  "/home/b/Documents/Dropbox/Programování/Geant4/Opt/src/OptRunAction.cc" "/home/b/Documents/Dropbox/Programování/Geant4/Opt/Build/CMakeFiles/Opt.dir/src/OptRunAction.cc.o"
+  "/home/b/Documents/Dropbox/Programování/Geant4/Opt/src/OptStackingAction.cc" "/home/b/Documents/Dropbox/Programování/Geant4/Opt/Build/CMakeFiles/Opt.dir/src/OptStackingAction.cc.o"
+  "/home/b/Documents/Dropbox/Programování/Geant4/Opt/src/OptTrackInfo.cc" "/home/b/Documents/Dropbox/Programování/Geant4/Opt/Build/CMakeFiles/Opt.dir/src/OptTrackInfo.cc.o"
+  "/home/b/Documents/Dropbox/Programování/Geant4/Opt/src/OptTrackerHit.cc" "/home/b/Documents/Dropbox/Programování/Geant4/Opt/Build/CMakeFiles/Opt.dir/src/OptTrackerHit.cc.o"
+  "/home/b/Documents/Dropbox/Programování/Geant4/Opt/src/OptTrackerSD.cc" "/home/b/Documents/Dropbox/Programování/Geant4/Opt/Build/CMakeFiles/Opt.dir/src/OptTrackerSD.cc.o"
+  "/home/b/Documents/Dropbox/Programování/Geant4/Opt/src/OptTrackingAction.cc" "/home/b/Documents/Dropbox/Programování/Geant4/Opt/Build/CMakeFiles/Opt.dir/src/OptTrackingAction.cc.o"
+  "/home/b/Documents/Dropbox/Programování/Geant4/Opt/src/_OptDetectorConstruction.cc" "/home/b/Documents/Dropbox/Programování/Geant4/Opt/Build/CMakeFiles/Opt.dir/src/_OptDetectorConstruction.cc.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# Preprocessor definitions for this target.
+set(CMAKE_TARGET_DEFINITIONS_CXX
+  "G4INTY_USE_QT"
+  "G4INTY_USE_XT"
+  "G4MULTITHREADED"
+  "G4UI_USE"
+  "G4UI_USE_QT"
+  "G4UI_USE_TCSH"
+  "G4UI_USE_XM"
+  "G4VERBOSE"
+  "G4VIS_USE"
+  "G4VIS_USE_OPENGL"
+  "G4VIS_USE_OPENGLQT"
+  "G4VIS_USE_OPENGLX"
+  "G4VIS_USE_OPENGLXM"
+  "G4VIS_USE_RAYTRACERX"
+  "G4_STORE_TRAJECTORY"
+  "QT_CORE_LIB"
+  "QT_GUI_LIB"
+  "QT_NO_DEBUG"
+  "QT_OPENGL_LIB"
+  "QT_PRINTSUPPORT_LIB"
+  "QT_WIDGETS_LIB"
+  )
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "/usr/local/include"
+  "/usr/local/include/Geant4"
+  "/usr/include/Geant4"
+  "../include"
+  "/usr/include/x86_64-linux-gnu/qt5"
+  "/usr/include/x86_64-linux-gnu/qt5/QtWidgets"
+  "/usr/include/x86_64-linux-gnu/qt5/QtGui"
+  "/usr/include/x86_64-linux-gnu/qt5/QtCore"
+  "/usr/lib/x86_64-linux-gnu/qt5/mkspecs/linux-g++-64"
+  "/usr/include/x86_64-linux-gnu/qt5/QtPrintSupport"
+  "/usr/include/x86_64-linux-gnu/qt5/QtOpenGL"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ Build/CMakeFiles/Opt.dir/cmake_clean.cmake
@@ -0,0 +1,21 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Opt.dir/Opt.cc.o"
+  "CMakeFiles/Opt.dir/src/OptPrimaryGeneratorAction.cc.o"
+  "CMakeFiles/Opt.dir/src/_OptDetectorConstruction.cc.o"
+  "CMakeFiles/Opt.dir/src/OptRunAction.cc.o"
+  "CMakeFiles/Opt.dir/src/OptStackingAction.cc.o"
+  "CMakeFiles/Opt.dir/src/OptTrackInfo.cc.o"
+  "CMakeFiles/Opt.dir/src/OptDetectorMessenger.cc.o"
+  "CMakeFiles/Opt.dir/src/OptActionInitialization.cc.o"
+  "CMakeFiles/Opt.dir/src/OptPhysicsList.cc.o"
+  "CMakeFiles/Opt.dir/src/OptTrackerHit.cc.o"
+  "CMakeFiles/Opt.dir/src/OptTrackingAction.cc.o"
+  "CMakeFiles/Opt.dir/src/OptTrackerSD.cc.o"
+  "Opt.pdb"
+  "Opt"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Opt.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
